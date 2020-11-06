116bdda81563a169a78b0f18fd708cc2666d44f9
aksmith5239 <aklobby@gmail.com>
2020-11-06 12:07:21 -0700

    uninstalling node_modules

--- models/index.js
+++ models/index.js
@@ -3,6 +3,16 @@ const Appointment = require('./Appointment');
 const Stylist = require('./Stylist');
 const Service = require('./Service');
 
+//associations
+// Stylist.hasMany(Service, {
+//     foreignKey: 'stylist_id'
+// });
+
+// Service.belongsTo(Stylist, {
+//     foreignKey: 'stylist_id'
+// });
+
+
 module.exports = {
     Customer,
     Appointment,
