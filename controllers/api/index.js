d22f50b723b5a671048643ea7898c49364932574
terahje <terahjequinn@gmail.com>
2020-11-07 13:57:21 -0700

    can get appointment and appointment by id

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
 const customerRoutes = require('./customer-routes.js');
+const appointmentRoutes = require('./appointment-routes.js');
+
 const stylistRoutes = require('./stylist-routes.js');
 
 router.use('/customer', customerRoutes);
+router.use('/appointment', appointmentRoutes);
 router.use('/stylists', stylistRoutes);
+
 
 
 module.exports = router;
