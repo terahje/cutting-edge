2755a3f56d1d860e7b9abec111d6a5b949fcaa8b
aksmith5239 <aklobby@gmail.com>
2020-11-08 08:59:53 -0700

    mvc structure in place

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', handlebars);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
