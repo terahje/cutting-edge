ff71eff5229779c6633d42ed0f234918803b2105
Jennifer Schlierman <jd9913@gmail.com>
2020-11-11 15:43:37 -0700

    update powerpoint

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -1,23 +1,16 @@
 
-var Calendar = require('tui-calendar'); 
-/* CommonJS */
-var DatePicker=require('tui-date-picker');
-var TimePicker=require('tui-time-picker');
 
 
-require("tui-calendar/dist/calendar.css");
 
 
-var calendar = new Calendar(('#calendar'), {
-    defaultView: 'month',
-    taskView: true,
-    template: {
-      monthDayname: function(dayname) {
-        return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
-      }
-      
-    }
-  });
+
+const calendarEl= document.getElementById(calendar);
+
+
+let calendar = new Calendar(calendarEl, {
+  plugins: [ dayGridPlugin ],
+  initialView: 'dayGridWeek'
+});
 
 
     module.exports=calendar;
